ac5c9b0c140f6c683a1177d19229ab8a580fda71
clarkgraves2 <71040960+clarkgraves2@users.noreply.github.com>
2025-03-11 11:10:51 -0400

    Setup server_config_t structure

--- include/config.h
+++ include/config.h
@@ -12,6 +12,24 @@
  
  #include <stdbool.h>
 
+/*************************************************************************
+ * Type Definitions
+ *************************************************************************/
 
+/**
+  * @brief Server configuration structure
+  */
+ typedef struct server_config 
+ {
+     // Network settings 
+     int port;                // Server listening port 
+     
+     // Thread management 
+     int num_worker_threads;  // Number of worker threads for the thread pool 
+     
+     // Logging configuration 
+     const char *log_file;    // Path to log file
+
+ } server_config_t;
 
  #endif
